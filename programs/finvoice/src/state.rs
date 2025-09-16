74fa4d70a151a81c9b2d38a80f81574f4e32f2c3
Khushal <khushal2004a@gmail.com>
2025-09-16 21:38:51 +0530

    fix: state

--- programs/finvoice/src/state.rs
+++ programs/finvoice/src/state.rs
@@ -2,21 +2,21 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct Invoice {
-    pub issuer: Pubkey,
-    pub payer: Option<Pubkey>,
-    pub invoice_amount: u64,
-    pub currency: u16,
-    pub due_date: i64,
-    pub status: u8, // 0=Created,1=Listed,2=Funded,3=Settled,4=Defaulted
-    pub mint_pubkey: Pubkey,
-    pub ipfs_cid: [u8; 46],
-    pub attestor_pubkey: Pubkey,
-    pub attestor_sig: [u8; 64],
-    pub created_at: i64,
+    pub issuer: Pubkey,          // 32
+    pub payer: Option<Pubkey>,   // 1 (discriminant) + 32 (Pubkey) = 33
+    pub invoice_amount: u64,     // 8
+    pub currency: u16,           // 2
+    pub due_date: i64,           // 8
+    pub status: u8,              // 1
+    pub mint_pubkey: Pubkey,     // 32
+    pub ipfs_cid: [u8; 46],      // 46
+    pub attestor_pubkey: Pubkey, // 32
+    pub attestor_sig: [u8; 64],  // 64
+    pub created_at: i64,         // 8
 }
 
 impl Invoice {
-    pub const SIZE: usize = 32 + 1 + 32 + 8 + 2 + 8 + 1 + 32 + 46 + 32 + 64 + 8;
+    pub const SIZE: usize = 32 + 33 + 8 + 2 + 8 + 1 + 32 + 46 + 32 + 64 + 8;
 }
 
 #[account]
